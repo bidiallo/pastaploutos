8bbc90acd35e7f88fdf8a802614186b5497926f2
CamilleLaThug <camille.paoletti74@gmail.com>
2017-11-24 15:35:48 +0100

    module connexion

---
+++ modules/mod_inscription/modele_inscription.php
@@ -0,0 +1,15 @@
+<?php 
+require_once ('./include/modele_generique.php');
+
+class ModeleInscription extends ModeleGenerique {
+
+	/* public __construct(){
+		parent:: __construct();
+	} */
+
+	function modele_inscription(){
+		//faire requete
+	}
+
+}
+
---
+++ modules/mod_inscription/vue_inscription.php
@@ -0,0 +1,28 @@
+<?php
+//require_once('include/vue_generique');
+
+class VueInscription extends VueGenerique{
+	 
+	function __construct(){
+		parent::__construct();
+	}
+	function vue_form_connexion(/*askip token*/){
+		?> 
+		<form action = "index.php?module=inscription&action=form_inscription" method="POST">
+
+			<!-- <input id="" type="" name=""/><br/> -->
+			<input id="nom" type="text" name="nom"/><br/>
+			<input id="prenom" type="text" name="prenom"/><br/>
+			<input id="pseudo" type="text" name="pseudo"/><br/>
+			<input id="email" type="email" name="email"><br/>
+			<input id="mdp" type="password" name="mdp"/><br/>
+			<input id="mdp2" type="password" name="mdp2"/><br/>
+			<input id="age" type="date" name="age"/><br/>
+			
+			<input type="submit" value="Envoyer"/>
+
+		</form><?php
+	}
+}
+
+?>
