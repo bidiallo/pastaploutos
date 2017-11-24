469b952330a195b97173cf3100255083c0441e3c
bidiallo <bineta.diallo08@gmail.com>
2017-11-24 18:09:20 +0100

    modif template

--- include/template.php
+++ include/template.php
@@ -20,5 +20,33 @@
 	</head>
 	<body>
 
+	<?php echo $module->getControleur()->getVue()->getContenu()?>
+
+
+	<!--footer-->
+		<footer>
+			
+			<div class="row">
+			  <div class="col-xs-6 col-sm-4">
+			  	<p> PastaPloutos est un site vouzs permettant de trouver n'importe quel recette à base de pate en passant par la traditionnelle recette des pates à la bolognaise et allant jusqu'à vous présenter les recettes les plus originales du moment.</p>
+			  	
+			  </div>
+
+			  <div class="col-xs-6 col-sm-4">Informations pratiques</div>
+
+			  <!-- Optional: clear the XS cols if their content doesn't match in height -->
+			  <div class="clearfix visible-xs-block"></div>
+
+			  <div class="col-xs-6 col-sm-4">
+			  	<ul>
+			  		<li><a href="#">Les origines du site</a></li>
+			  		<li><a href="#">Contact</a></li>
+			  		<li><a href="#">Mentions Légales</a></li>
+			  	</ul>
+			  </div>
+			</div>
+
+		</footer>
+
 	</body>
 </html>
